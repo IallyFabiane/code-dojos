fbb604c4a2582a638f4e3170d62a825748999fbe
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-04 11:53:44 -0300

    comentários explicativos da resolução do desafio sequência de cinco

--- sequencia_de_cinco.js
+++ sequencia_de_cinco.js
@@ -1,3 +1,10 @@
+//Resolução do desafio Sequência de Cinco do DojoPuzzles:
+//intNums é uma matriz quadrada (contém o mesmo número de linhas e colunas) e portanto, só tem duas diagonais (A principal e a secundária);
+//Diagonais de intNums: 
+//Diagonal Principal (intNums[0][0] = 5, intNums[1][1] = 5,  intNums[2][2] = 5,  intNums[3][3] = 5,  intNums[4][4] = 5);
+//Diagonal Secundária (intNums[0][4] = 9, intNums[1][3] = 8,  intNums[2][2] = 5,  intNums[3][1] = 7,  intNums[4][0] = 9);
+
+
 let intNums = [
     [5, 6, 7, 8, 9],
     [6, 5, 7, 8, 9],
@@ -6,9 +13,9 @@ let intNums = [
     [9, 8, 7, 6, 5]
 ];
 
-let prodVertical = intNums[0][4] * intNums[1][4] * intNums[2][4] * intNums[3][4] * intNums[4][4];
-console.log(prodVertical);
-let prodHorizontal = intNums[0][0] * intNums[0][1] * intNums[0][2] * intNums[0][3] * intNums[0][4];
-console.log(prodHorizontal);
-let prodDiagonal = intNums[0][4] * intNums[1][3] * intNums[2][2] * intNums[3][1] * intNums[4][0];
-console.log(prodDiagonal);
+let maiorProdutoVertical = intNums[0][4] * intNums[1][4] * intNums[2][4] * intNums[3][4] * intNums[4][4];
+console.log(maiorProdutoVertical);
+let maiorProdutoHorizontal = intNums[0][0] * intNums[0][1] * intNums[0][2] * intNums[0][3] * intNums[0][4];
+console.log(maiorProdutoHorizontal);
+let maiorProdutoDiagonal = intNums[0][4] * intNums[1][3] * intNums[2][2] * intNums[3][1] * intNums[4][0];
+console.log(maiorProdutoDiagonal);
