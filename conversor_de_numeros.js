ad534d603bebac53bfbc21deca805f94cff95800
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-10-05 09:57:07 -0300

    desafio intervalos

---
+++ conversor_de_numeros.js
@@ -0,0 +1,16 @@
+
+const algarismosRomanos = [
+   'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VII', 'IX', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXX', 'XC', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM', 'M'
+];
+
+const algarismosIndoArabicos = [
+   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000
+];
+
+console.log('escolha um algarismo romano: ')
+
+if (algarismosRomanos[0]) {
+   return console.log(algarismosIndoArabicos[0]);
+}
+         
+
---
+++ intervalos.js
@@ -0,0 +1,10 @@
+const nums = [100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150];
+
+const intervalos1 = nums.slice(0, 3);
+const intervalos2 = nums.slice(3, 6);
+const intervalos3 = nums.slice(6, 9);
+const intervalos4 = nums.slice(9, 12);
+
+const intervalos = [intervalos1, intervalos2, intervalos3, intervalos4]
+
+console.log(intervalos);
